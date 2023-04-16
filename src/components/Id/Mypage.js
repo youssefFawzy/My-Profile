ffeaa1ba674c958f3e067873be8accdda9f50d56
youssefFawzy <70449983+youssefFawzy@users.noreply.github.com>
2023-04-16 22:57:30 +0200

    Update Mypage.js

--- src/components/Id/Mypage.js
+++ src/components/Id/Mypage.js
@@ -5,6 +5,36 @@ function Id() {
   return (
     <div>
         <Particle />
+        <Container className="home-content">
+          <Row>
+            <Col md={7} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
+
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> Youssef Fawzy</strong>
+              </h1>
+
+              <div style={{ padding: 50, textAlign: "left" }}>
+                <Type />
+              </div>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col>
+          </Row>
+        </Container>
     </div>
   );
 }
