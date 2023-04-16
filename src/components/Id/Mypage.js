4cb23211ede654e04b91c5bdfc89bf973d1b28d8
youssefFawzy <70449983+youssefFawzy@users.noreply.github.com>
2023-04-16 09:48:45 +0200

    Update Mypage.js

--- src/components/Id/Mypage.js
+++ src/components/Id/Mypage.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Id() {
-  return (
-    <div>
-      <iframe src={process.env.PUBLIC_URL + "/page.html"} title="My HTML Page"></iframe>
-    </div>
-  );
+
 }
 
 export default Id;
