92ad4e79f0c6f274f4ad2302e0ae902d5b0a240b
youssefFawzy <70449983+youssefFawzy@users.noreply.github.com>
2023-04-16 06:48:14 +0200

    s

---
+++ src/components/Id/Home2.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+            I'm a graphic designer, passionate about creating visual experiences that leave a lasting impact.🤷‍♂️
+              <br />
+              <br />I am fluent in classics design tools like
+              <i>
+                <b className="purple"> Adobe Photoshop, Illustrator, and InDesign </b>
+              </i>
+              <br />
+              <br />
+              My field of Interests are building innovative&nbsp;
+              <i>
+                <b className="purple">visual designs and products </b>  for web and print media.{" "}
+              </i>
+              <br />
+              <br />
+              I'm also passionate about staying up-to-date with the latest design technologies, including 
+              <i>
+                <b className="purple"> 
+                  {" "}
+                  UI/UX design and responsive web.
+                </b>
+              </i>
+              &nbsp; and I apply this knowledge to my work with tools like
+              <i>
+                <b className="purple"> Figma and Sketch.</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/youssefFawzy"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/youssef_fawzy3"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
