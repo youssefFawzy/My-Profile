15ffa53f8d0dede4750de4b08d886324a73af7c5
youssefFawzy <70449983+youssefFawzy@users.noreply.github.com>
2023-04-16 22:58:45 +0200

    Update Mypage.js

--- src/components/Id/Mypage.js
+++ src/components/Id/Mypage.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
+import Home2 from "./Home2";
+import Type from "./Type";
 
 function Id() {
   return (
