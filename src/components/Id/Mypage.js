6d6e8b9abc0ea7f27ac0e3c752806464cb20e206
youssefFawzy <70449983+youssefFawzy@users.noreply.github.com>
2023-04-17 05:15:35 +0200

    Update Mypage.js

--- src/components/Id/Mypage.js
+++ src/components/Id/Mypage.js
@@ -33,7 +33,7 @@ function Id() {
         <div id="profile" className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
           <div className="p-4 md:p-12 text-center lg:text-left">
             {/* Image for mobile view*/}
-            <div className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style={{backgroundImage: 'url("https://source.unsplash.com/MP0IUfwrn0A")'}} />
+            <div className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style={{backgroundImage: 'url("https://media.discordapp.net/attachments/947191234047410277/1097359140650758164/2023-01-26_012324.514.JPG?width=493&height=657")'}} />
             <h1 className="text-3xl font-bold pt-8 lg:pt-0">Youssef Fawzy</h1>
             <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25" />
             <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start"><svg className="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" /></svg> Graphic Designer</p>
@@ -58,7 +58,7 @@ function Id() {
         {/*Img Col*/}
         <div className="w-full lg:w-2/5">
           {/* Big profile image for side bar (desktop) */}
-          <img src="https://source.unsplash.com/MP0IUfwrn0A" className="rounded-none lg:rounded-lg shadow-2xl hidden lg:block" />
+          <img src="https://media.discordapp.net/attachments/947191234047410277/1097359140650758164/2023-01-26_012324.514.JPG?width=493&height=657" className="rounded-none lg:rounded-lg shadow-2xl hidden lg:block" />
           {/* Image from: http://unsplash.com/photos/MP0IUfwrn0A */}
         </div>
       </div>
